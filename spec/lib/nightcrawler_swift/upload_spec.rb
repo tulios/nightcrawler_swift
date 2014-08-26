be667dddb89a3a65679f6ef9e637ee2dc07cf963
tulios <ornelas.tulio@gmail.com>
2014-08-26 16:13:38 -0300

    better return tests for upload command

--- spec/lib/nightcrawler_swift/upload_spec.rb
+++ spec/lib/nightcrawler_swift/upload_spec.rb
@@ -9,9 +9,11 @@ describe NightcrawlerSwift::Upload do
   describe "#execute" do
     let(:path) { "file_name" }
     let(:file) { double(:file, read: 'content') }
+
     let(:connection) do
       double(:connection, upload_url: 'server-url')
     end
+
     let(:response) do
       double(:response, code: 201)
     end
@@ -19,19 +21,39 @@ describe NightcrawlerSwift::Upload do
     before do
       allow(NightcrawlerSwift).to receive(:connection).and_return(connection)
       allow(subject).to receive(:put).and_return(response)
+    end
+
+    let :execute do
       subject.execute path, file
     end
 
     it "reads file content" do
+      execute
       expect(file).to have_received(:read)
     end
 
     it "sends file content as body" do
+      execute
       expect(subject).to have_received(:put).with(anything, body: 'content')
     end
 
     it "sends to upload url with given path" do
+      execute
       expect(subject).to have_received(:put).with("server-url/file_name", anything)
+    end
+
+    context "when response code is 200" do
+      let(:response) { double(:response, code: 200) }
+      it { expect(execute).to be true }
+    end
+
+    context "when response code is 201" do
+      it { expect(execute).to be true }
+    end
+
+    context "when response code is different than 200 or 201" do
+      let(:response) { double(:response, code: 500) }
+      it { expect(execute).to be false }
     end
   end
 
