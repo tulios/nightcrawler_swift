ef080d83fc892fc6c7b436bd3d11a2007d1b9403
tulios <ornelas.tulio@gmail.com>
2014-09-22 22:00:27 -0300

    small refactoring in command

--- lib/nightcrawler_swift/command.rb
+++ lib/nightcrawler_swift/command.rb
@@ -21,25 +21,28 @@ module NightcrawlerSwift
 
     def get url, args = {}
       prepare_args args
-      Gateway.new(url).request {|r| r.get args[:headers].merge(params: args[:params])}
+      Gateway.new(url).request {|r| r.get headers_and_params(args)}
     end
 
     def put url, args = {}
       prepare_args args
-      Gateway.new(url).request {|r| r.put args[:body], args[:headers].merge(params: args[:params])}
+      Gateway.new(url).request {|r| r.put args[:body], headers_and_params(args)}
     end
 
     def delete url, args = {}
       prepare_args args
-      Gateway.new(url).request {|r| r.delete args[:headers].merge(params: args[:params])}
+      Gateway.new(url).request {|r| r.delete headers_and_params(args)}
     end
 
     private
-
     def prepare_args args
       args[:headers] ||= {}
       args[:headers]["X-Storage-Token"] = connection.token_id
       args[:params] ||= {}
+    end
+
+    def headers_and_params args
+      args[:headers].merge(params: args[:params])
     end
   end
 end
